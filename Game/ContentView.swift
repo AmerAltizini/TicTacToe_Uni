de175625212de3032831f0887e0bf88191abbfe6
AmerAltizini <94787298+AmerAltizini@users.noreply.github.com>
2022-02-06 19:52:20 +0100

    Initial commit

---
+++ Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game
+//
+//  Created by M1 Mac 1 on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by M1 Mac 1 on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
